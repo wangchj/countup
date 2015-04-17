e0562f0cbff714258b5588800cfcbb8afc27d4b6
wangchj <cjw39@hotmail.com>
2015-04-17 13:01:30 -0500

    Track start date in History table instead of Counters table.

--- data/db.sql
+++ data/db.sql
@@ -38,19 +38,17 @@ create table Counters (
     counterId integer     primary key,
     userId    integer     not null,
     label     varchar(30) not null,
-    startDate text        not null,
     summary   text        null,               -- Describe this counter
     timeZone  text        null,               -- Timezone of this counter. If null, user's default timezone should be used.
     longest   integer     not null default 0, -- Longest day streak
     public    integer     not null default 1,
-    active    integer     not null default 1,
     foreign key(userId) references Users(userId)
 );
 
 create table History (
-    counterId integer not null,
-    startDate text not null,
-    endDate   text not null,
+    counterId integer   not null,
+    startDate text      not null,
+    endDate   text      null,       -- If null, this is the current count of the counter
     primary key (counterId, startDate, endDate),
     foreign key(counterId) references Counters(counterId)
 );
