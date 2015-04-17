b6b058d7129847f57b11196436144d6e1297f6c6
wangchj <cjw39@hotmail.com>
2015-04-17 02:30:33 -0500

    SVG calendar interface (HTML and JavaScript).

--- views/site/home.js
+++ views/site/home.js
@@ -1,6 +1,7 @@
 $(function() {
-    initWindowResizing();
-    resizeBoxFigure();
+    //initWindowResizing();
+    //resizeBoxFigure();
+    drawFigures();
 });
 
 var prevWidth; //Figure container width before resizing
@@ -21,6 +22,93 @@ function windowResized() {
         resizeBoxFigure();
         prevWidth = w;
     }    
+}
+
+var numcol  = 7; //Number of columns in this calendar
+var gutter  = 10; //Spacing between months in pixels
+var space   = 2; //Spacing between cells in pixels
+
+/**
+ * Draw a figure for all counters.
+ */
+function drawFigures() {
+    var now = new Date();
+    console.log(now);
+    console.log(now.getFullYear());
+    $('svg').each(function() {
+        drawFigure(this, now.getFullYear(), now.getMonth());
+    });
+}
+
+/**
+ * Draw a figure, which contains multiple calendar.
+ */
+function drawFigure(svg, year, month) {
+    var width = Math.min($('.cimg').width() - 50, 900); //Width of the figure
+    var snap = Snap(svg);
+
+    //snap.rect(50, 50, 50, 50);
+    var g1, g2;
+
+    if(month === 0) {
+        g1 = drawMonthCalendar(snap, 0, width, year - 1, 11); //December of last year
+    }
+    else {
+        g1 = drawMonthCalendar(snap, 0, width, year, month - 1);
+    }
+
+    var g2 = drawMonthCalendar(snap, 1, width, year, month);
+
+    var tx = (+g1.attr('width')) + gutter;
+    g2.transform('t' + tx);
+}
+
+/**
+ * Draw a calendar.
+ * @param snap      Snap object
+ * @param i         calendar index: the first calendar is 0, next is 1.
+ * @param cwidth    figure (canvas) total width.
+ * @param year      year, as in 2014
+ * @param month     month: 0 = Jan, 1 = Feb, ... , 11 = Dec
+ */
+function drawMonthCalendar(snap, i, cwidth, year, month) {
+    var numdays = numberOfDays(year, month + 1); //Number of days in this month;
+    var startOn = (new Date(year, month, 1)).getDay(); //Day of the first of this month; 0 = Sunday, 1 = Monday, etc
+    var numrow  = Math.ceil((numdays + startOn) / numcol); //Number of rows in this calendar
+    var width   = (((cwidth - gutter) / 2) - ((numcol - 1) * space)) / numcol; //width of cell
+
+    console.log('year: ' + year);
+    console.log('month: ' + month);
+    console.log('numdays: ' + numdays);
+    console.log('startOn: ' + startOn);
+    console.log('numcol: ' + numcol);
+    console.log('numrow: ' + numrow);
+    console.log('gutter: ' + gutter);
+    console.log('space: ' + space);
+    console.log('width: ' + width);
+
+    var group = snap.group();
+    group.attr({width: width * numcol + space * numcol, height: width * numrow + space * numrow});
+
+    for(row = 0; row < numrow; row++) {
+        for(col = 0; col < numcol; col++) {
+            var x = col * width + col * space;
+            var y = row * width + row * space;
+            var s = row * numcol + col;
+            //console.log(s);
+            //console.log(startOn);
+            var c = s < startOn || s > numdays + startOn ? '#eeeeee' : '#d6e685';
+            var rect = snap.rect(x, y, width, width).attr({fill:c});
+            group.add(rect);
+        }
+    }
+
+    return group;
+}
+
+function numberOfDays(year, month) {
+    var d = new Date(year, month, 0);
+    return +d.getDate();
 }
 
 function resizeBoxFigure() {
--- views/site/home.php
+++ views/site/home.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Url;
 use yii\web\AssetBundle;
+use app\lib\DateTime;
+
 //use app\views\site\HomeAsset;
 
 class HomeAsset extends AssetBundle
@@ -15,78 +17,6 @@ class HomeAsset extends AssetBundle
         'app\assets\SnapsvgAsset',
         'yii\web\JqueryAsset',
     ];
-}
-
-class ColorShifter {
-    private $R = 0, $G = 1, $B = 2;     //Color constant code
-    private $c;                    //Color that is currently changing
-    private $t = 1;
-    private $ar = 0, $ag = 30, $ab = 30; //Current angle for r, g, b.
-    private $div;                       //How many portion to divide 90 degrees
-    private $diff;                      //Degrees per portion
-    private $min;
-    private $max;
-
-    public function __construct($d=25, $min=180, $max=255) {
-        //150-200, 180-255
-        $this->c = $this->R;
-        $this->div = $d;
-        $this->diff = 90 / $this->div;
-        $this->min = $min;
-        $this->max = $max;
-    }
-
-    /**
-     * Change to next hue. Return nothing. Call get getColor() to get a color.
-     */
-    public function changeHue() {
-        if($this->c == $this->R)
-            $this->ar += $this->diff;
-        else if($this->c == $this->G)
-            $this->ag += $this->diff;
-        else
-            $this->ab += $this->diff;
-
-        if($this->t == $this->div)
-            $this->c = ($this->c + 1) % 3;
-
-        $this->t = ($this->t + 1) % ($this->div + 1);
-    }
-
-    /**
-     * Return HTML color code.
-     */
-    public function getColor($level) {
-        $r = sin(deg2rad($this->ar)) * ($this->max - $this->min) + $this->min;
-        $g = sin(deg2rad($this->ag)) * ($this->max - $this->min) + $this->min;
-        $b = sin(deg2rad($this->ab)) * ($this->max - $this->min) + $this->min;
-
-        $level_fact = 15;
-
-        $r += -1 * $level * $level_fact;
-        $g += -1 * $level * $level_fact;
-        $b += -1 * $level * $level_fact;
-
-        return $this->rgb2html($r, $g, $b);
-    }
-
-    public static function rgb2html($r, $g=-1, $b=-1)
-    {
-        if (is_array($r) && sizeof($r) == 3)
-            list($r, $g, $b) = $r;
-
-        $r = intval($r); $g = intval($g);
-        $b = intval($b);
-
-        $r = dechex($r<0?0:($r>255?255:$r));
-        $g = dechex($g<0?0:($g>255?255:$g));
-        $b = dechex($b<0?0:($b>255?255:$b));
-
-        $color = (strlen($r) < 2?'0':'').$r;
-        $color .= (strlen($g) < 2?'0':'').$g;
-        $color .= (strlen($b) < 2?'0':'').$b;
-        return '#'.$color;
-    }  
 }
 
 HomeAsset::register($this);
@@ -179,58 +109,39 @@ nav.navbar {
             You currently have no counters. Click on Add Counter to add one!
         </div>
     </div>
-<?php else: ?><?php foreach(Yii::$app->user->identity->counters as $counter):?>
-    <div class="row" style="
-        /*border:1px solid gray;*/
-        background-color:#fff;
-        padding:20px;
-        font-size:18px;
-        font-weight:bold;
-        color:#505050;
-        box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.22);
-        border-radius: 6px;
-        margin-bottom:20px;
-        ">
-        <center>
-        <div class="col-sm-12 col-md-12 col-lg-12" style="">
-            <div class="row clabel">
-                <div class="col-md-12 col-lg-12" style=""><?=$counter->label?></div>
-            </div>
-            <hr>
-            <div class="row">
-                <!--div class="col-md-3 col-lg-3"><?=$counter->startDate?></div -->
-                <div class="col-md-3 col-lg-3 col-md-offset-3"><?=$counter->getDays()?> Days</div>
-                <div class="col-md-3 col-lg-3"><?=$counter->longest?> Days</div>
-            </div>
-            <hr>
-            <div class="row cimg">
-                <div class="col-md-12 col-lg-12">
-                    <?php
-                        $w = 10; //Rec width in pixels
-                        $s = 2;  //Spacing in pixels
-                        $a = $w * 51 + $s * 51;
-                        $b = $w * 6 + $s * 6;
-
-                        $cs = new ColorShifter();
-                    ?>
-                    <svg width="<?=$a?>" height="<?=$b?>" style="visibility:visible;opacity:.8">
-                        <?php for($col = 0; $col < 52; $col++): ?>
-                            <?php for($row = 0; $row < 7; $row++): ?>
-                                <?php
-                                    $id = $col * 7 + $row;
-                                    $x  = $col * $w + $col * $s; //Horizontal position
-                                    $y  = $row * $w + $row * $s; //Vertical position
-                                    $co = $counter->getDays() >= $id + 1 ? $cs->getColor($row) : '#eeeeee';
-                                ?>
-                                <rect id="d<?=$id?>" width="<?=$w?>" height="<?=$w?>" x="<?=$x?>" y="<?=$y?>" fill="<?=$co?>" data-s="<?=$id?>" rx="2", ry="2" />
-                            <?php endfor;?>
-                            <?php $cs->changeHue();?>
-                        <?php endfor;?>
-                    </svg>
+<?php else: ?>
+    <div class="row">
+        <?php foreach(Yii::$app->user->identity->counters as $counter):?>
+            <div class="col-sm-4 col-md-4" style="padding-top:15px; padding-bottom:15px"> <!-- Produce gutters between counters -->
+                <div style="
+                    /*border:1px solid gray;*/
+                    background-color:#fff;
+                    padding:15px 20px;
+                    font-size:18px;
+                    font-weight:bold;
+                    color:#505050;
+                    box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.22);
+                    border-radius: 6px;
+                    /*margin-bottom:20px;*/
+                    text-align:center;
+                    ">
+                    <div class="row clabel">
+                        <div class="col-md-12 col-lg-12" style=""><?=$counter->label?></div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <!--div class="col-md-3 col-lg-3"><?=$counter->startDate?></div -->
+                        <div class="col-md-6 col-lg-6"><?=$counter->getDays()?> Days</div>
+                        <div class="col-md-6 col-lg-6"><?=$counter->longest?> Days</div>
+                    </div>
+                    <hr>
+                    <div class="row cimg">
+                        <center>
+                            <svg id="cal<?=$counter->counterId?>"></svg>
+                        </center>
+                    </div>
                 </div>
             </div>
-        </div>
-        </center>
-    </div>
-<?php endforeach;?><?php endif;?>
+        <?php endforeach;?>
+<?php endif;?>
 </div>
