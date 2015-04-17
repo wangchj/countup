ccc0d7e2b55bc1695cf3ed5ffde832863ee4b384
wangchj <cjw39@hotmail.com>
2015-04-17 14:46:25 -0500

    Add some testing db data.

--- data/test-data.sql
+++ data/test-data.sql
@@ -1,9 +1,16 @@
-INSERT INTO Users VALUES(1,'Jeff','cjw39@hotmail.com','$2y$10$KO5HdM03CdSOrpfBGJEKNedmvN1yLGNEE9w6AK73O.0QdEg6qlPAi','2014-08-22 19:50:49','America/Chicago','weMW5GI3aQbaV-IgsdE3VS8Wish83uFI');
-INSERT INTO Counters VALUES(1,1,'Test Counter 1','2014-08-01','Test functionality of this app.',1,1);
-INSERT INTO Counters VALUES(2,1,'Graduate School','2010-01-01','How long I have attended graduate school.',1,1);
+INSERT INTO Users VALUES(1,'Jeff','Jeff', 'Wang', 'me@test.com', null, '$2y$10$KO5HdM03CdSOrpfBGJEKNedmvN1yLGNEE9w6AK73O.0QdEg6qlPAi','2014-08-22 19:50:49', null, 'America/Chicago', null, 'weMW5GI3aQbaV-IgsdE3VS8Wish83uFI');
 
-insert into History values(1, '2012-1-1', '2012-1-30');
-insert into History values(1, '2013-1-1', '2013-1-10');
-insert into History values(1, '2013-1-10', '2013-1-30');
-insert into History values(1, '2014-9-1', '2014-9-18');
-insert into History values(2, '2009-1-1', '2009-8-5');
+INSERT INTO Counters VALUES(1,1,'Test Counter 1','Test functionality of this app.','America/Chicago',0,1);
+INSERT INTO Counters VALUES(2,1,'Graduate School','How long I have attended graduate school.','America/Chicago',0,1);
+
+insert into History values(1, '2012-01-01', '2012-01-30');
+insert into History values(1, '2013-01-01', '2013-01-10');
+insert into History values(1, '2015-03-03', '2015-03-10');
+insert into History values(1, '2015-03-15', '2015-03-20');
+insert into History values(1, '2015-03-21', '2015-03-21');
+insert into History values(1, '2015-03-21', '2015-03-25');
+insert into History values(1, '2015-04-10', '2015-04-11');
+insert into History values(1, '2015-04-15', null); 
+insert into History values(1, '2013-01-10', '2013-01-30');
+insert into History values(1, '2014-09-01', '2014-09-18');
+insert into History values(2, '2009-01-01', '2009-08-05');
