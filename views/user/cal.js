7a732ffe848b564d98156770e3e1fd8f9d704917
wangchj <cjw39@hotmail.com>
2015-05-06 10:22:39 -0500

    Move functions markClicked() and getCellState() for marking calendar into cal.js.

--- views/user/cal.js
+++ views/user/cal.js
@@ -48,6 +48,64 @@ function initFigureEvents() {
     });
 }
 
+function markClicked(action, counterId, date) {    
+    //Make sure we're not marking date in the future.
+    var markDate = new Date(date);
+    var today = new Date();
+    if(dateGreater(markDate, today))
+        return;
+
+    var state = getCellState(counterId, date);
+    //console.log(state);
+
+    if(action == state || state == markError || action < 0 || action > 2)
+        return;
+
+    $.ajax({
+        type:'GET',
+        url: markUrl + '?action=' + action + '&counterId=' + counterId + '&date=' + date,
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(textStatus + ':' + errorThrown);
+        },
+        success: function(data, textStatus, jqXHR) {
+            //console.log(this.url);
+            //console.log('mark ajax success:' + textStatus);
+            Snap.select("g.cell[counter='" + counterId + "']" + "[date='" + date + "'] rect").
+                animate({fill: action == markNone ? colorNone : action == markDone ? colorDone : colorMiss}, 400);
+            
+            $.ajax({
+                type:'GET',
+                url: getDaysUrl + '?counterId=' + counterId,
+                error: function(jqXHR, textStatus, errorThrown) {
+                    console.log('get-days ajax ' + textStatus + ':' + errorThrown);
+                },
+                success: function(data, textStatus, jqXHR) {
+                    $('div#counter-container-' + counterId + ' ' + 'span.current-count').text(data);
+                }
+            });
+        }
+    });
+}
+
+function getCellState(counterId, date) {
+    var cell = $("g.cell[counter='" + counterId + "']" + "[date='" + date + "']");
+    var rect = cell.find('rect');
+    var color = rect.attr('fill');
+
+    //console.log(cell);
+
+    switch(color) {
+        case colorDone:
+            return markDone;
+        case colorNone:
+            return markNone;
+        case colorMiss:
+            return markMiss;
+        default:
+            return markError;
+    }
+}
+
 function initWindowResizing() {
     prevWidth = $('.cimg').width();
 
